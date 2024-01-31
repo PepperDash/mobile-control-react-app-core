bb7be32e01c6f6172a2e030a6b20c737f177e541
Neil Dorin <ndorin@pepperdash.com>
2024-01-30 20:12:28 -0700

    feat: adds clock and updates MainLayou to be able to hide volume

--- src/lib/shared/layout/habanero/MainLayout.tsx
+++ src/lib/shared/layout/habanero/MainLayout.tsx
@@ -7,12 +7,12 @@ import classes from './MainLayout.module.scss';
  * @param param0 
  * @returns 
  */
-export const MainLayout = ({header, footer, content, volume}: MainLayoutProps) => {
+export const MainLayout = ({header, footer, content, volume, showVolume}: MainLayoutProps) => {
   return (
     <div className={classes.grid}>
       <div className={classes.header}>{header}</div>
       <div className={classes.content}>{content}</div>
-      <div className={classes.volume}>{volume}</div>
+      {showVolume && <div className={classes.volume}>{volume}</div>}
       <div className={classes.footer}>{footer}</div>
     </div>
     );
@@ -23,4 +23,5 @@ interface MainLayoutProps {
   footer: React.ReactNode;
   content: React.ReactNode;
   volume: React.ReactNode;
+  showVolume?: boolean;
 }
