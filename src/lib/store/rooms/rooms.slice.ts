6110741cf1ade490cebfa83f1a4139e551f78b6e
Andrew Welker <awelker@pepperdash.com>
2024-03-04 12:11:10 -0600

    fix: update room reducer to match message from MC EPI

--- src/lib/store/rooms/rooms.slice.ts
+++ src/lib/store/rooms/rooms.slice.ts
@@ -11,29 +11,31 @@ const roomsSlice = createSlice({
     initialState,
     reducers: {
         setRoomState(state, action:PayloadAction<Message>) {
-            const type = action.payload.type;
+            const type = action.payload.type;            
+            const key = type.slice(type.lastIndexOf('/') + 1);            
 
-            // extract the room key from the type
-            const matches = type.match('/room/(.*)/status');
+            console.log(type, key);
 
-            if (!matches) return;
-
-            const key = matches[1];
+            if(!key) return;
             
             // This method solves the issue of multiple layers of properties
             // and avoids doing a deep copy of the object
 
             const content = action.payload.content as RoomState;
 
+            console.log(content);
+
             // Get existing room state
             const existingState = state[key] ?? {};
 
             // merge new state with existing
-            const newState = _.merge(existingState, content);
+            const newState = merge(existingState, content);
 
             // overlay the incoming state properties onto the existing item
             // or create new item
             state[key] = newState;
+
+            console.log(state);
 
             return state;
         }
