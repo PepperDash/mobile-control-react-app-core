9a5a13e9486e291a192738ee61d7b72faed20c63
Neil Dorin <ndorin@pepperdash.com>
2024-05-30 10:47:27 -0600

    feat: adds useSystemControl hook for program reboot/reset

--- src/lib/shared/hooks/index.ts
+++ src/lib/shared/hooks/index.ts
@@ -3,4 +3,5 @@ export * from "./useDeviceIBasicVolumeWithFeedback";
 export * from "./useGetDeviceStateFromRoomConfiguration";
 export * from "./usePressHoldRelease";
 export * from "./useRoomIBasicVolumeWithFeedback";
+export * from "./useSystemControl";
 export * from "./useTimeAndDate";
