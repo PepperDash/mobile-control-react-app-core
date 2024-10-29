45956b8d7746ceb221a48e1fc0b78070c526c094
Neil Dorin <ndorin@pepperdash.com>
2024-10-29 11:35:18 -0600

    feat: add temporary interface to extend AppConfig for additional app metadata

--- src/lib/store/appConfig/appConfigSelectors.ts
+++ src/lib/store/appConfig/appConfigSelectors.ts
@@ -1,6 +1,8 @@
+import { AppConfigExtras } from 'src/lib/types';
 import { useAppSelector } from '../hooks';
 
-export const useAppConfig = () => useAppSelector((state) => state.appConfig.config);
+// Temporary cast to AppConfigExtras until the data is moved to a websocket message
+export const useAppConfig = () => useAppSelector((state) => state.appConfig.config) as AppConfigExtras;
 
 export const useApiPath = () => useAppSelector((state) => state.appConfig.config.apiPath);
 
