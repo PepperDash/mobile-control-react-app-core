af561108eecc4553433d02e6cf718e4e0fd26084
Andrew Welker <awelker@pepperdash.com>
2025-06-09 09:05:23 -0500

    fix: update disconnected message

--- src/lib/shared/disconnectedMessage/DisconnectedMessage.tsx
+++ src/lib/shared/disconnectedMessage/DisconnectedMessage.tsx
@@ -1,19 +1,36 @@
-import { useError, useShowReconnect, useWebsocketContext } from "src/lib";
+import {
+  useError,
+  useShowReconnect,
+  useWebsocketContext,
+  useWsIsConnected,
+} from 'src/lib';
 import classes from './DisconnectedMessage.module.scss';
 
 const DisconnectedMessage = () => {
   const { reconnect } = useWebsocketContext();
+  const isConnected = useWsIsConnected();
   const errorMessage = useError();
   const showReconnect = useShowReconnect();
 
-
-  return<>
-    <div className={`disconnected-message ${classes.mwfit} mx-auto text-center`}>
-      <h1>Disconnected</h1>      
-      {errorMessage && <h5>{errorMessage}</h5>}
-      {showReconnect && <button className="btn btn-secondary btn-lg" onClick={reconnect}>Reconnect</button>}
-    </div>
-  </>
-}
+  return (
+    <>
+      <div
+        className={`disconnected-message ${classes.mwfit} mx-auto text-center`}
+      >
+        {isConnected === undefined ? (
+          <h1>Connecting...</h1>
+        ) : (
+          <h1>Disconnected</h1>
+        )}
+        {errorMessage && <h5>{errorMessage}</h5>}
+        {showReconnect && (
+          <button className="btn btn-secondary btn-lg" onClick={reconnect}>
+            Reconnect
+          </button>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default DisconnectedMessage;
