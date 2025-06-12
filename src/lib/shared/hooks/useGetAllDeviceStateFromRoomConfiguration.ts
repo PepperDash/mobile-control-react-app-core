e43be543915c8ae44d0fd452edb3052dca391f0e
Neil Dorin <ndorin@pepperdash.com>
2025-06-12 12:21:10 -0600

    fix: streamline device key collection in useGetAllDeviceStateFromRoomConfiguration hook and prevent duplicates

--- src/lib/shared/hooks/useGetAllDeviceStateFromRoomConfiguration.ts
+++ src/lib/shared/hooks/useGetAllDeviceStateFromRoomConfiguration.ts
@@ -1,89 +1,102 @@
-import { useEffect } from "react";
+import { useEffect } from 'react';
 import { RoomConfiguration } from 'src/lib/types/state/state';
 import { useWebsocketContext } from 'src/lib/utils/useWebsocketContext';
 
-
 /**
  * This hook will gather up all the keys for devices in the room
- * and  send messages to the websocket to get the iniital state 
+ * and  send messages to the websocket to get the iniital state
  * for each device
  */
-export const useGetAllDeviceStateFromRoomConfiguration = ({config}: {config: RoomConfiguration | undefined}) => {
+export const useGetAllDeviceStateFromRoomConfiguration = ({
+  config,
+}: {
+  config: RoomConfiguration | undefined;
+}) => {
   const { sendMessage } = useWebsocketContext();
 
   useEffect(() => {
     if (!config) {
-      return
+      return;
     }
 
-    const deviceKeys = []
-   
+    const deviceKeys: string[] = [];
+
+    // Helper function to push device keys into the array and avoid duplicates
+    // Also, avoid pushing '$off' as a device key
+    function pushDeviceKey(key: string) {
+      if (key && key !== '$off' && !deviceKeys.includes(key)) {
+        deviceKeys.push(key);
+      }
+    }
+
     if (config.destinations) {
       Object.values(config.destinations).forEach((d) => {
-        deviceKeys.push(d);
+        pushDeviceKey(d);
       });
     }
 
     if (config.destinationList) {
       Object.values(config.destinationList).forEach((dli) => {
-        deviceKeys.push(dli.sinkKey);
+        pushDeviceKey(dli.sinkKey);
       });
     }
 
-    if(config.audioControlPointList) {
-      Object.values(config.audioControlPointList?.levelControls).forEach((lcl) => {
-        // if the level control has an item key, combine it with the parent device key
-        if(lcl.itemKey) {
-          deviceKeys.push(lcl.parentDeviceKey + "--" + lcl.itemKey);
-        } else {
-          deviceKeys.push(lcl.parentDeviceKey);
+    if (config.audioControlPointList) {
+      Object.values(config.audioControlPointList?.levelControls).forEach(
+        (lcl) => {
+          // if the level control has an item key, combine it with the parent device key
+          if (lcl.itemKey) {
+            pushDeviceKey(lcl.parentDeviceKey + '--' + lcl.itemKey);
+          } else {
+            pushDeviceKey(lcl.parentDeviceKey);
+          }
         }
-      });
+      );
     }
 
     config.touchpanelKeys?.forEach((d) => {
-      deviceKeys.push(d);
+      pushDeviceKey(d);
     });
 
     config.environmentalDevices?.forEach((d) => {
-      deviceKeys.push(d.deviceKey);
+      if (d.deviceKey) pushDeviceKey(d.deviceKey);
     });
 
     config.accessoryDeviceKeys?.forEach((d) => {
-      deviceKeys.push(d);
+      pushDeviceKey(d);
     });
 
     if (config.audioCodecKey) {
-      deviceKeys.push(config.audioCodecKey);
+      pushDeviceKey(config.audioCodecKey);
     }
 
     if (config.videoCodecKey) {
-      deviceKeys.push(config.videoCodecKey);
+      pushDeviceKey(config.videoCodecKey);
     }
 
     if (config.matrixRoutingKey) {
-      deviceKeys.push(config.matrixRoutingKey);
+      pushDeviceKey(config.matrixRoutingKey);
     }
 
     if (config.roomCombinerKey) {
-      deviceKeys.push(config.roomCombinerKey);
+      pushDeviceKey(config.roomCombinerKey);
     }
 
     if (config.endpointKeys) {
       config.endpointKeys.forEach((ek) => {
-        deviceKeys.push(ek);
+        pushDeviceKey(ek);
       });
     }
-    
+
     if (config.sourceList) {
       for (const value of Object.values(config.sourceList)) {
         // if the source has a source key, add it to the list of device keys
-        if (value.sourceKey && value.sourceKey !== "$off")
-          deviceKeys.push(value.sourceKey);
+        if (value.sourceKey && value.sourceKey !== '$off')
+          pushDeviceKey(value.sourceKey);
       }
     }
 
-    console.log("deviceKeys", deviceKeys);
+    console.log('requesting state for deviceKeys:', deviceKeys);
 
     deviceKeys.forEach((dk) => {
       sendMessage(`/device/${dk}/fullStatus`, { deviceKey: dk });
