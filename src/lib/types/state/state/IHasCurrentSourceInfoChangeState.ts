000600394ad04072d944612063dd74b149abe793
Andrew Welker <awelker@pepperdash.com>
2025-07-17 14:45:35 -0500

    fix: make currentSource{s} optional

--- src/lib/types/state/state/IHasCurrentSourceInfoChangeState.ts
+++ src/lib/types/state/state/IHasCurrentSourceInfoChangeState.ts
@@ -10,11 +10,11 @@ export interface IHasCurrentSourceInfoChangeState extends DeviceState {
    * The key of the current source.
    * This is used to identify which source is currently active.
    */
-  currentSourceKey: string;
+  currentSourceKey?: string;
 
   /**
    * The current source item.
    * This provides detailed information about the currently active source.
    */
-  currentSource: SourceListItem;
+  currentSource?: SourceListItem;
 }
