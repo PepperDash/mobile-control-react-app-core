77b265e8dbaa07d028b14ad0bba0a6a8079498c2
Neil Dorin <ndorin@pepperdash.com>
2025-07-18 14:37:57 -0600

    feat: adds SecondaryAudio to SignalType

--- src/lib/shared/hooks/interfaces/useIRunDirectRouteAction.ts
+++ src/lib/shared/hooks/interfaces/useIRunDirectRouteAction.ts
@@ -1,29 +1,36 @@
 import { useWebsocketContext } from '../../../utils/useWebsocketContext';
 
 /**
- * 
+ *
  * @param roomKey key of the room
- * @returns 
+ * @returns
  */
-export function useIRunDirectRouteAction(roomKey: string): IRunDirectRouteActionProps {
-    const { sendMessage } = useWebsocketContext();
+export function useIRunDirectRouteAction(
+  roomKey: string
+): IRunDirectRouteActionProps {
+  const { sendMessage } = useWebsocketContext();
 
-    const runDirectRoute = (route: DirectRoute) => {
-        sendMessage(`/room/${roomKey}/directRoute`, route );
-    };
+  const runDirectRoute = (route: DirectRoute) => {
+    sendMessage(`/room/${roomKey}/directRoute`, route);
+  };
 
-    return { runDirectRoute };
+  return { runDirectRoute };
 }
 
 interface IRunDirectRouteActionProps {
-    runDirectRoute: (route: DirectRoute) => void;
+  runDirectRoute: (route: DirectRoute) => void;
 }
 
 interface DirectRoute {
-    sourceKey: string;
-    destinationKey: string;
-    signalType: SignalType;
+  sourceKey: string;
+  destinationKey: string;
+  signalType: SignalType;
 }
 
-export type SignalType = "Audio" | "Video" | "AudioVideo" | "UsbOutput" | "UsbInput";
-
+export type SignalType =
+  | 'Audio'
+  | 'Video'
+  | 'AudioVideo'
+  | 'UsbOutput'
+  | 'UsbInput'
+  | 'SecondaryAudio';
