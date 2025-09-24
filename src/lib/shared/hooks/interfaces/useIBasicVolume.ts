6d52412a31832df3c73c04e682a4a0bbc1728c7c
jkdevito <jdevito@pepperdash.com>
2025-09-24 09:57:57 -0500

    feat: add IBasicVolumeControl hooks
    - Implemented to handle volume scenarios without feedback
    - Extends useDeviceIBasicVolumeWIthFeedback
    - Extends useRoomIBasicVolume

---
+++ src/lib/shared/hooks/interfaces/useIBasicVolume.ts
@@ -0,0 +1,47 @@
+import { useWebsocketContext } from '../../../utils/useWebsocketContext';
+import { useButtonHeldHeartbeat } from '../useButtonHeldHeartbeat';
+import { PressHoldReleaseReturn } from '../usePressHoldRelease';
+
+/**
+ * hook to control a volume device that implements the IBasicVolumeControls interface
+ * @param path path prefix to for the device. i.e. /device/{key} or /room/{key}
+ * @returns
+ */
+export function useIBasicVolume(
+  path: string,
+): IBasicVolumeReturn | undefined {
+  const { sendMessage, sendSimpleMessage } = useWebsocketContext();
+
+  const volumeUp = useButtonHeldHeartbeat(`${path}`, 'volumeUp');
+  const volumeDown = useButtonHeldHeartbeat(`${path}`, 'volumeDown');
+
+  const muteToggle = () => sendMessage(`${path}/muteToggle`, null);
+
+  return {
+    volumeUp,
+    volumeDown,
+    muteToggle,
+  };
+}
+
+export interface IBasicVolumeReturn {
+  volumeUp: PressHoldReleaseReturn;
+  volumeDown: PressHoldReleaseReturn;
+  muteToggle: () => void;
+}
+
+export function useGetIBasicVolumeWithFeedback(
+  path: string,
+): IBasicVolumeReturn | undefined {
+  const { sendMessage, sendSimpleMessage } = useWebsocketContext();
+
+  const volumeUp = useButtonHeldHeartbeat(`${path}`, 'volumeUp');
+  const volumeDown = useButtonHeldHeartbeat(`${path}`, 'volumeDown');
+
+  const muteToggle = () => sendMessage(`${path}/muteToggle`, null);
+  return {
+    volumeUp,
+    volumeDown,
+    muteToggle,
+  };
+}
