2adf5f81612ec1f92878d544abb58d32ff79d76f
Neil Dorin <ndorin@pepperdash.com>
2025-09-25 12:40:18 -0600

    feat: implement useStateIsSynced hook and related UI state management

---
+++ src/lib/shared/hooks/useStateIsSynced.ts
@@ -0,0 +1,22 @@
+import { uiActions, useAppDispatch, useIsSyncStateValuePresent } from 'src/lib';
+
+/**
+ * Custom hook to manage synchronization state for a specific component.
+ * @param name The name of the component to synchronize state for.
+ * @returns [() => setSyncState, () => removeSyncState, boolean: isSynced]
+ */
+export function useStateIsSynced(
+  name: string
+): [() => void, () => void, boolean] {
+  const dispatch = useAppDispatch();
+
+  const setSyncState = () => {
+    dispatch(uiActions.addSyncState(name));
+  };
+
+  const removeSyncState = () => {
+    dispatch(uiActions.removeSyncState(name));
+  };
+
+  return [setSyncState, removeSyncState, useIsSyncStateValuePresent(name)];
+}
